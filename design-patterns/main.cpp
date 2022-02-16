868f74c0bb1c2c656b3f057e90813c2952b204d6
xin <xin.li.phy@gmail.com>
2022-02-15 21:10:47 -0500

    Initial Commit

---
+++ design-patterns/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  design-patterns
+//
+//  Created by Xin Li on 2/15/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
